6e5c066a09d27868f7d15b40ebb767861122ace9
ken restivo <krestivo@restivo.org>
2012-05-20 00:22:24 -0700

    defpage-hack

--- src/crudite/core.clj
+++ src/crudite/core.clj
@@ -1,6 +1,20 @@
-(ns crudite.core)
+(ns crudite.core
+  (:use
+   [hiccup.core :only [html]]
+   [hiccup.form :only [form-to label submit-button
+                       text-field file-upload drop-down]]
+   [noir.response :only [redirect]]
+   [noir.core :only [defpage defpartial render]])
+  (:require
+   [noir.session :as session]
+   [noir.validation :as validation]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+
+
+(defmacro defpage-hack
+  "Wraps noir.core/defpage, after eval'ing the url argument,
+    so that you can pass the url as (str something something) or (:keyname m)
+    instead of a static string or vector."
+  [url des & all]
+  (let [cleaned (eval url)]
+  `(defpage ~cleaned ~des ~@all)))
