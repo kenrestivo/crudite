59eb4c289087fb6527e8f16ca12fa402a08a3c8f
ken restivo <krestivo@restivo.org>
2012-05-28 00:12:46 -0700

    start of units

--- test/crudite/unit.clj
+++ test/crudite/unit.clj
@@ -2,7 +2,21 @@
   (:use clojure.test
         crudite.core
         crudite.actions)
-  (:require [crudite.settings :as settings]))
+  (:require [crudite.settings :as settings]
+            [noir.util.test]))
 
 
 
+
+(doseq [a [add-post-action update-post-action]]
+  (noir.util.test/with-noir
+    (a settings/defaults {:id 1 :foo "bar" :baz "quuz"}))
+
+  (noir.util.test/with-noir
+    (a (settings/merge-fsettings {:form-valid? (constantly false)})
+       {:id 1 :foo "bar" :baz "quuz"})))
+
+
+(doseq [a [add-get-action edit-get-action view-one-action]]
+  (noir.util.test/with-noir
+    (a settings/defaults {:id 1})))
