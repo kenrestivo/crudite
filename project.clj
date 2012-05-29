87a16358f25cb9f229d7256c98357cb8a8faf0bb
ken restivo <krestivo@restivo.org>
2012-05-28 22:56:31 -0700

    make it compile with lein 1.7.1

--- project.clj
+++ project.clj
@@ -6,9 +6,8 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta7"]]
   :dev-dependencies [[clj-http "0.4.1"] ;; needs to be explicit!?
-                           [com.cemerick/url "0.0.6"]
-                           [com.cemerick/pomegranate "0.0.4"]]
+                     [com.cemerick/url "0.0.6"]
+                     ]
   :profiles {:dev {:dependencies [[clj-http "0.4.1"] ;; needs to be explicit!?
-                           [com.cemerick/url "0.0.6"]
-                           [com.cemerick/pomegranate "0.0.4"]]}})
+                                  ]}})
 
